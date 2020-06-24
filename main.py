845bc3d3bacf7e1840605596367ffc115fd3f6bd
Domepo <36802235+Domepo@users.noreply.github.com>
2020-06-24 22:05:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from PIL import Image
+
+#quality from 0-100
+image_quality = 50
+
+input_keyboard = input(r"directory: ")
+input_path = input_keyboard
+
+#create folder for compressed images
+os.mkdir(input_path+"/converted")
+
+#search for JPGS,PNGS
+
+for file in os.listdir(input_path):
+    filename, file_extension = os.path.splitext(file)
+    if (file_extension == ".JPG" or file_extension == ".png" or file_extension == ".jpeg"):
+        big_image = Image.open(input_path+"/"+file)
+        converted_image = big_image.save(input_path+"/Konvertiert/"+filename+"-small.jpeg",quality=image_quality)
+        
+print("finished")
