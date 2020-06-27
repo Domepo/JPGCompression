8b6f9d4e0010b07fb613106267abdca8051910f7
Domepo <36802235+Domepo@users.noreply.github.com>
2020-06-27 10:41:57 +0200

    Update multiplefolders.py

--- test/multiplefolders.py
+++ test/multiplefolders.py
@@ -1,29 +1,31 @@
 import os
 from PIL import Image
 
-#quality from 0-100
-image_quality = 50
-output_extension = "jpeg"
 
 input_keyboard = input(r"directory: ")
-input_path = input_keyboard
-
-#create folder for compressed images
 
 
-#search for JPGS,PNGS
+def img_compressor(input_path,name_of_new_folder,end_name,image_quality,output_extension):
 
-for ordner in os.listdir(input_path):
-    aa = os.mkdir(input_path+"/"+ordner+"/converted")
-    print(aa)
-    for file in os.listdir(input_path + "/" +ordner):
-        filename, file_extension = os.path.splitext(file)
-        if (file_extension == ".JPG" or file_extension == ".png" or file_extension == ".jpeg" or file_extension == ".jpg"):
-            big_image = Image.open(input_path+"/"+ordner+"/"+file)
-            converted_image = big_image.save(input_path+"/"+ordner+"/converted/"+filename+"-small."+output_extension,quality=image_quality)
 
-for ordner in os.listdir(input_keyboard):
-    print(ordner)
-    for file in os.listdir(input_path + "/"+ordner):
-        print("test"+file)
-print("finished")
+    for folder in os.listdir(input_path):
+        os.mkdir(input_path+"/"+folder+"/"+name_of_new_folder)  
+        #create folder for compressed images
+        counter = 0
+        print("folder: "+folder+" finished")    
+        
+        for file in os.listdir(input_path +"/"+folder):
+            counter = counter + 1
+            filename, file_extension = os.path.splitext(file)
+
+            if (file_extension == ".JPG" or file_extension == ".png" or file_extension == ".jpeg" or file_extension == ".jpg"):
+                #add some more extensions if needed
+                big_image = Image.open(input_path+"/"+folder+"/"+file)
+                converted_image = big_image.save(input_path+"/"+folder+"/"+name_of_new_folder+"/"+folder+"-"+str(counter)+"-"+end_name+"."+output_extension,quality=image_quality)
+                #quality from 0-100
+            
+
+img_compressor(input_keyboard,"aonvertiert"," knv",1,"jpeg")
+
+print("********finished********")
+
